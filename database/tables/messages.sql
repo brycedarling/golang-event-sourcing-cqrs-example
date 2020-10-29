da2801a473729adea26399019e4ce06839ac8217
Bryce Darling <darlingbryce@gmail.com>
2020-10-29 15:46:20 -0400

    Add database initialization sql files

---
+++ database/tables/messages.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS message_store.messages (
+  id UUID NOT NULL DEFAULT gen_random_uuid(),
+  stream_name text NOT NULL,
+  type text NOT NULL,
+  position bigint NOT NULL,
+  global_position bigserial NOT NULL,
+  data jsonb,
+  metadata jsonb,
+  time TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc') NOT NULL
+);
+
+ALTER TABLE message_store.messages ADD PRIMARY KEY (global_position) NOT DEFERRABLE INITIALLY IMMEDIATE;
